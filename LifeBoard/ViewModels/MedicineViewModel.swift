0c7a1c984336ad3c30bfdd02c7ad9f8a98ce8918
Esma Koçak <esma.kocaakk20@gmail.com>
2025-02-20 20:07:56 +0300

    Medicine UI & CoreData

---
+++ LifeBoard/ViewModels/MedicineViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MedicineViewModel.swift
+//  LifeBoard
+//
+//  Created by Esma Koçak on 19.02.2025.
+//
+
+import Foundation
---
+++ LifeBoard/Views/AddMedicineView.swift
@@ -0,0 +1,18 @@
+//
+//  AddMedicineView.swift
+//  LifeBoard
+//
+//  Created by Esma Koçak on 20.02.2025.
+//
+
+import SwiftUI
+
+struct AddMedicineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMedicineView()
+}
